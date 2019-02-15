ebdb907f51cd16bf61002b5469976ece97aa27d1
Kothique Félixx <kothique@protonmail.ch>
2019-02-15 19:49:00 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [ '@babel/preset-env' ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
